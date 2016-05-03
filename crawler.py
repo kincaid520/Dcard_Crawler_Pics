adba155764e1954fbb5b8e92c65e20bf82ea92c6
kincaid520 <kincaid520@hotmail.com>
2016-05-03 18:54:42 +0800

    logic improvent
    search all id in all specific pages first instead of searching next page after searching links

--- crawler.py
+++ crawler.py
@@ -33,28 +33,28 @@ def get(PAGES=PAGES, FORUM=FORUM, POPULAR=False):
 			post_id.append(post['id'])
 	
 
-		# find every link in every article
-		for id in post_id:
-			print(">Searching in ID %d"%id)
-			r = requests.get('https://www.dcard.tw/api/post/all/'+str(id))# this website is fixed
-			article = r.json()
-			content =  article['version'][0]['content'] # looking into the content
-			p = re.compile(ur'(http:\/\/i?.?imgur.com\/[\w]+)')
-			result= re.findall(p,content)
-			for i in result:
-				print("  Capturing picture from %s"%i)
-				second_r = requests.get(i)
-				second_content = second_r.content
-				second_p = re.compile(ur'(http:\/\/i?.?imgur.com\/\w+\.[jpeng]+)')
-				res_pic = re.findall(second_p, second_content)
-				post_link.append(res_pic[0])
+	# find every link in every article
+	for id in post_id:
+		print(">Searching in ID %d"%id)
+		r = requests.get('https://www.dcard.tw/api/post/all/'+str(id))# this website is fixed
+		article = r.json()
+		content =  article['version'][0]['content'] # looking into the content
+		p = re.compile(ur'(http:\/\/i?.?imgur.com\/[\w]+)')
+		result= re.findall(p,content)
+		for i in result:
+			print("  Capturing picture from %s"%i)
+			second_r = requests.get(i)
+			second_content = second_r.content
+			second_p = re.compile(ur'(http:\/\/i?.?imgur.com\/\w+\.[jpeng]+)')
+			res_pic = re.findall(second_p, second_content)
+			post_link.append(res_pic[0])
 
-		# save to file
-		if SAVETOFILE:
-			file=open('output','w')
-			for each in post_link:
-				file.write(str(each)+'\n')
-			file.close()
+	# save to file
+	if SAVETOFILE:
+		file=open('output','w')
+		for each in post_link:
+			file.write(str(each)+'\n')
+		file.close()
 
 	return post_link
 
